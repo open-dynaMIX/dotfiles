5a44a5f8ce774db4c4cb5e622d83430f6fca1e41
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2017-09-22 10:11:27 +0200

    add some ant files

---
+++ dotfiles/scripts/radio/radio.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+INPUT="${DIR}/radio.csv"
+OLDIFS=$IFS
+IFS=';'
+[ ! -f "$INPUT" ] && { echo "$INPUT file not found"; exit 99; }
+declare -A table=()
+table_index=()
+while read -r title url; do
+    table["$title"]="$url"
+    table_index+=("$title")
+done < "$INPUT"
+IFS=$OLDIFS
+
+select station in "${table_index[@]}"; do
+    break
+done
+
+if [[ "${table[$station]}" == *.pls ]] || [[ "${table[$station]}" == *.m3u ]]; then
+    mplayer -cache 1024 -vo none -playlist "${table[$station]}"
+else
+    mplayer -cache 1024 -vo none "${table[$station]}"
+fi
