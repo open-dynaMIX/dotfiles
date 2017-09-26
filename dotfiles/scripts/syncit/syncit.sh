da35774c569d62ef4bb565fb3efdf3093e11e564
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2017-09-26 17:46:53 +0200

    add syncit

---
+++ dotfiles/scripts/syncit/syncit.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+#
+## syncit
+## Version 0.1
+##
+## 2013
+## 2014
+## Fabio Rämi
+
+
+echo "
+ ____ ____ ____ ____ _________ ____ ____
+||S |||Y |||N |||C |||   ☠   |||I |||T ||
+||__|||__|||__|||__|||_______|||__|||__||
+|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|
+
+"
+
+## variables
+scriptlocation=$(readlink -f $0)
+scriptdir=$(dirname $scriptlocation)
+rsynccmd="rsync -avxe ssh --delete --delete-excluded --include-from=/home/{{@@ env['USER'] @@}}/scripts/syncit/include --exclude-from=/home/{{@@ env['USER'] @@}}/scripts/syncit/exclude"
+serverdir="{{@@ env['USER'] @@}}@{{@@ env['backup_host'] @@}}:/var/disk/{{@@ env['USER'] @@}}/backup_{{@@ env[profile + '_model'] @@}}/"
+localdir="/home/{{@@ env['USER'] @@}}/"
+
+rm -f "$scriptdir"/.pyoutput
+
+# close file descriptors and cleanup temp files
+function cleanup {
+        if [ "$zenity" == true  ]; then
+          exec 3>&-
+          exec 4>&-
+          rm -f $scriptdir/.pyoutput
+        fi
+        rm -f /tmp/syncitfifo.$$
+}
+
+function usage {
+        echo -e "usage: $0 [--zenity|--shut|-h]"
+}
+
+# handle missing slash at the end of localdir
+if ! echo "$localdir" | egrep -q "/$"; then
+  localdir="$localdir/"
+fi
+
+for var in "$@"
+do
+    if [ $var == '-h' ]; then
+      usage
+      exit 0
+    elif [ $var == '--zenity' ]; then
+      zenity=true
+    elif [ $var == '--shut' ]; then
+      shut=true
+    fi
+done
+
+# redirect stdout & stderr to logfile if there is not terminal
+if ! [[ -t 1 ]] || [ "$zenity" == true ]; then
+  zenity=true
+  exec 1>>$scriptdir/syncit.log 2>&1
+else
+  zenity=false
+fi
+
+# Check if all needed commands are available.
+# Taken from http://wiki.bash-hackers.org/scripting/style
+if [ "$zenity" == true  ]; then
+  my_needed_commands="zenity gksudo rsync"
+else
+  my_needed_commands="rsync"
+fi
+missing_counter=0
+for needed_command in $my_needed_commands; do
+  if ! hash "$needed_command" >/dev/null 2>&1; then
+    printf "Command not found: %s\n" "$needed_command" >&2
+    ((missing_counter++))
+  fi
+done
+if ((missing_counter > 0)); then
+  printf "$(date) - Minimum %d commands are missing, aborting.\n" "$missing_counter" >&2
+  exit 1
+fi
+
+# fetch the sudo password
+if ! [[ -t 1 ]]; then
+  while ! sudo -kSp '' [ 1 ] <<<"${sudopassword}" 2>/dev/null; do
+    sudopassword="$( gksudo --print-pass --message 'Provide permission for the process to shut down your computer.
+Type your password, or press Cancel.' -- : 2>/dev/null )"
+    # Check for null entry or cancellation.
+    if [[ ${?} != 0 || -z ${sudopassword} ]]
+    then
+        exit 1
+    fi
+  done
+else
+  if [ "$shut" == true ]; then
+    echo -n "Provide permission for the process to shut down your computer.
+Password:"
+    read -s sudopassword; echo
+  fi
+fi
+
+if [ "$zenity" == true  ]; then
+  # prepare the notifications
+  exec 4> >(zenity --notification --listen)
+  exec 3> >($scriptdir/trayicon.py)
+  echo "icon:$scriptdir/icon.png" >&4
+  echo "icon $scriptdir/icon.png" >&3
+
+  # create the file for python to write in
+  rm -f $scriptdir/.pyoutput
+  touch $scriptdir/.pyoutput
+fi
+
+# the rsync part
+echo "$(date) - Sync is in progress..."
+if [ "$zenity" == true  ]; then
+  echo "tooltip sync is in progress" >&3
+  echo "message:Sync is in progress..." >&4
+fi
+
+# Using fifo to get the PID of rsync.
+FIFONAME=/tmp/syncitfifo.$$
+rm -f $FIFONAME
+mkfifo $FIFONAME
+# The rsync-command
+$rsynccmd $localdir $serverdir > "$FIFONAME" &
+RSYNC_PID=$!
+
+# execute rsync
+while read -r; do
+  LINE="$REPLY"
+  if [ -e "$localdir$LINE" ] && [[ -t 1 ]]; then
+    if [ "$zenity" == true  ]; then
+      echo "tooltip Syncing: $LINE" >&3
+    fi
+  fi
+  echo -e "$LINE"
+  # check if user wants to cancel
+  if [ -f "$scriptdir/.pyoutput" ]; then
+    if cat "$scriptdir/.pyoutput" | egrep -q "quit"; then
+      kill $RSYNC_PID
+      echo "$(date) - Cancel"
+      if [ "$zenity" == true  ]; then
+        echo "message:Cancel" >&4
+      fi
+      sleep 2
+      cleanup
+      exit 1
+    fi
+  fi
+done <"$FIFONAME"
+
+# fetch and react on the rsync exit status
+wait $RSYNC_PID
+rsync_stat=$?
+
+case $rsync_stat in
+  0)
+    echo "$(date) - Syncing was successful."
+    if [ "$zenity" == true  ]; then
+      echo "message:Syncing was successfull" >&4
+    fi
+  ;;
+  *)
+    echo "$(date) - Error while syncing! Exit $rsync_stat"
+    if [ "$zenity" == true  ]; then
+      echo "message:Error while syncing! Exit $rsync_stat" >&4
+    fi
+    sleep 1
+    cleanup
+    exit 1
+  ;;
+esac
+
+# check if user wants to shutdown the computer
+if [ "$zenity" == true  ]; then
+  pyshut=($( cat $scriptdir/.pyoutput ))
+fi
+if [ -n "$pyshut" ]; then
+  count=$(echo -e "$pyshut" | wc -l)
+  ((count--))
+  for item in "$pyshut"; do
+    case ${pyshut[$count]} in
+      noshut)
+        exitnow="yes"
+        break
+      ;;
+      turnoff)
+        exitnow="no"
+        break
+      ;;
+      *)
+        exitnow="yes"
+    esac
+    ((count--))
+  done
+elif [ -n "$shut" ]; then
+  :
+else
+  exitnow="yes"
+fi
+if [ "$exitnow" = "yes" ]; then
+  cleanup
+  exit 0
+fi
+
+# execute shutdown as root
+echo "$(date) - Shutting down in 3 seconds."
+if [ "$zenity" == true  ]; then
+  echo "tooltip Shutting down in 3 seconds" >&3
+  echo "message:Shutting down in 3 seconds" >&4
+fi
+sleep 3
+cleanup
+echo 'echo -e "$sudopassword\n" | sudo -S -s -- shutdown -h now'
+exit 0
---
+++ dotfiles/scripts/syncit/trayicon.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python2
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#
+
+import gtk
+import gobject
+import sys
+import threading
+import os
+
+
+class TrayIcon(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.icon = gtk.StatusIcon()
+        self.icon.set_visible(False)
+        self.current_icon = None
+        self.icon.connect("popup-menu", self.right_click_event)
+        # self.icon.connect("button-press-event", self.left_click_event)
+        self.icon.connect('activate', self.left_click_event)
+        self.shutdownstatus = True
+
+    def left_click_event(self, icon, button):
+        print "yes"
+
+    def right_click_event(self, icon, button, time):
+        menu = gtk.Menu()
+
+        if self.shutdownstatus:
+            noshut = gtk.MenuItem("No shutdown")
+            noshut.connect("activate", self.noshutdown)
+            menu.append(noshut)
+        else:
+            noshut = gtk.MenuItem("Shutdown when finished")
+            noshut.connect("activate", self.noshutdown)
+            menu.append(noshut)
+
+        cancel = gtk.MenuItem("Cancel")
+        cancel.connect("activate", self.exodos)
+        menu.append(cancel)
+
+        menu.show_all()
+
+        menu.popup(None, None, gtk.status_icon_position_menu, button, time,
+                   self.icon)
+
+    def noshutdown(self, dummy):
+        if self.shutdownstatus:
+            pyfile = open((sys.path[0]) + "/.pyoutput", "wb")
+            pyfile.write("noshut")
+            pyfile.close()
+            self.shutdownstatus = False
+        else:
+            pyfile = open((sys.path[0]) + "/.pyoutput", "wb")
+            pyfile.write("turnoff")
+            pyfile.close()
+            self.shutdownstatus = True
+
+    def exodos(self, dummy):
+        pyfile = open((sys.path[0]) + "/.pyoutput", "wb")
+        pyfile.write("quit")
+        pyfile.close()
+
+    def run(self):
+        while True:
+            l = sys.stdin.readline()
+            if not l:
+                break
+            else:
+                fields = l.strip().split()
+                if not fields:
+                    continue
+                if fields[0] == "icon":
+                    gobject.idle_add(self.update_icon, fields[1])
+                elif fields[0] == "reload":
+                    gobject.idle_add(self.reload_icon)
+                elif fields[0] == "tooltip":
+                    gobject.idle_add(self.update_tooltip, " ".join(fields[1:]))
+                elif fields[0] == "hide":
+                    gobject.idle_add(self.hide)
+        os._exit(0)
+
+    def update_icon(self, fname):
+        if fname != self.current_icon:
+            self.icon.set_from_file(fname)
+            self.current_icon = fname
+        self.icon.set_visible(True)
+
+    def update_tooltip(self, tooltip):
+        self.icon.set_tooltip_text(tooltip)
+
+    def hide(self):
+        self.icon.set_visible(False)
+
+    def reload_icon(self):
+        self.icon.set_from_file(self.current_icon)
+        self.icon.set_visible(True)
+
+if __name__ == "__main__":
+    gobject.threads_init()
+    TrayIcon().start()
+    try:
+        gtk.main()
+    except KeyboardInterrupt:
+        os._exit(0)
