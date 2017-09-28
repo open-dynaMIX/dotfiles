0e3039173ef074bec100a661655a07876ef73ea8
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2017-09-28 09:57:31 +0200

    add wrapper

---
+++ dotfiles/scripts/wrapper.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+
+# handling arguments
+arglist=("--swap" "--unclut" "--both")
+if ! [[ "${arglist[@]}" =~ $1 ]] || ! [ -n "$2" ]; then
+    printf "Usage: ./wrapper.sh [--swap|--unclut|--both] COMMAND [ARGUMENTS]
+--swap   =  swap the mouse-buttons
+--unclut =  kill unclutter
+--both   =  do both\n"
+    if [[ "$1" =~ ^(-h|--help)$ ]]; then
+        exit 0
+    else
+        exit 1
+    fi
+fi
+
+# pre command
+if [[ "$1" =~ ^(--swap|--both)$ ]]; then
+    echo "Switching mouse-buttons"
+    # xmodmap -e "pointer = 1 2 3"
+    synclient TapButton1=1 TapButton2=3 TapButton3=2
+fi
+
+if [[ "$1" =~ ^(--unclut|--both)$ ]]; then
+    unclutrun=false
+    if pgrep unclutter; then
+        unclutrun=true
+        echo "Kill unclutter"
+        killall unclutter
+    else
+        echo "Unclutter is not running. Hence not killing and restarting it."
+    fi
+fi
+
+# command
+echo "Running command..."
+"${@:2}"
+
+# post command
+if [[ "$1" =~ ^(--swap|--both)$ ]]; then
+    echo "Switching back mouse-buttons"
+    # xmodmap -e "pointer = 3 2 1"
+    synclient TapButton1=3 TapButton2=1 TapButton3=2
+fi
+
+if [[ "$1" =~ ^(--unclut|--both)$ ]]; then
+    if $unclutrun; then
+        echo "Start unclutter"
+        unclutter -notclass -noevents Nemo Main.py Atom Feh Thunderbird Skype Pidgin \
+        Hedgewars Keepassx GCompris XTerm i3bar &
+    fi
+fi
