2df621597bcfb4926475957540e0620b176e424f
Fabio <open-dynaMIX@users.noreply.github.com>
2017-10-24 22:04:18 +0200

    add i3lock.sh

---
+++ dotfiles/i3/i3lock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This file is managed by dotdrop, only edit it in your dotdrop files!
+
+/usr/bin/i3lock -t -c 000000 -i ~/.i3/wallpaper/makepkgnotwarallblack2.png
+/usr/bin/setxkbmap ch
