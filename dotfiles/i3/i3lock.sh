26aa0a6c73f2907ccd5e8ad611bc0f748181f28a
Fabio <open-dynaMIX@users.noreply.github.com>
2017-10-25 08:35:44 +0200

    add default i3lock wallpaper

--- dotfiles/i3/i3lock.sh
+++ dotfiles/i3/i3lock.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # This file is managed by dotdrop, only edit it in your dotdrop files!
 
-/usr/bin/i3lock -t -c 000000 -i ~/.i3/wallpaper/makepkgnotwarallblack2.png
+/usr/bin/i3lock -t -c 000000 -i {{@@ env['i3lock_wallpaper'] @@}}
+{%@@ if env['greek_keylayout'] == 'true' @@%}
+sleep .8
 /usr/bin/setxkbmap ch
+{%@@ endif @@%}
