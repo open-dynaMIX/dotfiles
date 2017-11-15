761463f52a9449619ac2df39ca149e6dbe9123e2
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2017-11-15 10:57:30 +0100

    pause and resume dunst on screenlock

--- dotfiles/i3/i3lock.sh
+++ dotfiles/i3/i3lock.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # {{@@ env['dotdrop_warning'] @@}}
 
-/usr/bin/i3lock -t -c 000000 -i {{@@ env['i3lock_wallpaper'] @@}}
+# suspend message display
+killall -SIGUSR1 dunst
+
+/usr/bin/i3lock -n -t -c 000000 -i {{@@ env['i3lock_wallpaper'] @@}}
+
+# resume message display
+killall -SIGUSR2 dunst
+
 {%@@ if env['multiple_keylayouts'] == 'true' @@%}
 sleep 1.5
 /usr/bin/setxkbmap ch
