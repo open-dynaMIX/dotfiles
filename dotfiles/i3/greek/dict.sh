e6584715074469f9c1004e3e69b6e9f2c34a99d6
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2017-11-24 12:47:44 +0100

    add greek trainer

---
+++ dotfiles/i3/greek/dict.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# {{@@ env['dotdrop_warning'] @@}}
+#
+# Vokabeln von http://www.vokabeln.de/v5/vorschau/Griechisch_Alltag.htm
+# bis und mit Basiswortschatz: Abschnitt 1 bis 10: Abschnitt 10
+
+
+IFS='%'
+entry=($(grep -v "^#" ~/.i3/greek/basiswortschatz.csv | shuf | tail -n 1))
+unset IFS
+echo "German: ${entry[0]}"
+echo "Greek: ${entry[1]}"
