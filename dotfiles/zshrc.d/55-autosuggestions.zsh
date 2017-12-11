8b34269a5748a75132fb927d8563eab017dd9134
Fabio <open-dynaMIX@users.noreply.github.com>
2017-12-11 10:29:06 +0100

    fix slow pasting because of autosuggestions

--- dotfiles/zshrc.d/55-autosuggestions.zsh
+++ dotfiles/zshrc.d/55-autosuggestions.zsh
@@ -3,5 +3,8 @@
 # {{@@ env['dotdrop_warning'] @@}}
 #
 
+# Fix slow pasting
+zstyle ':bracketed-paste-magic' active-widgets '.self-*'
+
 . /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=59"
