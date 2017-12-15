1b9fe9157c520cb13a2068ad23ebae4e99ffc82b
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2017-12-15 08:35:31 +0100

    hack_no_transparency

--- dotfiles/i3/i3restart.sh
+++ dotfiles/i3/i3restart.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # {{@@ env['dotdrop_warning'] @@}}
 
+
+function hack_no_transparency {
+    xdotool search --onlyvisible --class "$1" | while read -r line; do
+        transset-df --id "$line" 1.0
+    done
+}
+
 i3-msg restart
 killall dunst
 sleep 0.3
 transset-df -n "Notes.txt - (~)" 0.8
-transset-df -n "Mozilla Thunderbird$" 1.0
-xdotool search --onlyvisible --class "Firefox" | while read -r line; do
-    transset-df --id "$line" 1
-done
+
+hack_no_transparency "Thunderbird"
+hack_no_transparency "Firefox"
+hack_no_transparency "Rambox"
