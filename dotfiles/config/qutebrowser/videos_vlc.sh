82a16c80ea29e694ceedf629c929bfc88506ab79
Fabio <open-dynaMIX@users.noreply.github.com>
2017-12-19 20:47:02 +0100

    make videos_vlc.sh act according to host

--- dotfiles/config/qutebrowser/videos_vlc.sh
+++ dotfiles/config/qutebrowser/videos_vlc.sh
@@ -3,4 +3,8 @@
 # If this is executed directly from within qutebrowser with spawn,
 # the browser crashes.
 
-youtube-dl -o - "$1" | vlc -
+if echo "$1" | grep "youtube.com"; then
+    vlc "$1"
+else
+    xterm -hold -e "youtube-dl -o - ${1} | vlc -"
+fi
