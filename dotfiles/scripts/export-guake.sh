373aca981aba32a0d6e317ef7a3a18ab18c1557d
Fabio <open-dynaMIX@users.noreply.github.com>
2017-12-20 20:24:06 +0100

    terminal fonts; fix export-guake.sh

--- dotfiles/scripts/export-guake.sh
+++ dotfiles/scripts/export-guake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gconftool-2 --dump /apps/guake > ~/.config/guake/apps-guake.xml
-gconftool-2 --dump /schemas/apps/guake > ~/.config/guake/schemas-apps-guake.xml
+gconftool-2 --dump /apps/guake > ~/dotfiles/dotfiles/config/guake/apps-guake.xml
+gconftool-2 --dump /schemas/apps/guake > ~/dotfiles/dotfiles/config/guake/schemas-apps-guake.xml
 
 echo "Done."
