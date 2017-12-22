3278c28bcbd12df7d296d17bcdb6760bf26ca690
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2017-12-22 09:51:27 +0100

    try to settle with a terminal font size

--- scripts/guake-export.sh
+++ scripts/guake-export.sh
@@ -2,5 +2,7 @@
 
 gconftool-2 --dump /apps/guake > ~/dotfiles/dotfiles/config/guake/apps-guake.xml
 gconftool-2 --dump /schemas/apps/guake > ~/dotfiles/dotfiles/config/guake/schemas-apps-guake.xml
+cp ~/dotfiles/dotfiles/config/guake/apps-guake.xml ~/.config/guake/apps-guake.xml
+cp ~/dotfiles/dotfiles/config/guake/schemas-apps-guake.xml ~/.config/guake/schemas-apps-guake.xml
 
 echo "Done."
