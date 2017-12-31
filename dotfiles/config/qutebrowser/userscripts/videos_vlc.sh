6ba42a8c4519f0c914b91cfdd6ee39236b41c0d2
Fabio <open-dynaMIX@users.noreply.github.com>
2017-12-31 15:22:13 +0100

    remove comment

--- dotfiles/config/qutebrowser/userscripts/videos_vlc.sh
+++ dotfiles/config/qutebrowser/userscripts/videos_vlc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # {{@@ env['dotdrop_warning'] @@}}
-# If this is executed directly from within qutebrowser with spawn,
-# the browser crashes.
 
 if echo "$QUTE_URL" | grep "youtube.com"; then
     vlc "$QUTE_URL"
