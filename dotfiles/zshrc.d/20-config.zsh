bc46d2d468a3b45f49bf9eb4202e29680676ce73
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2018-03-20 10:36:05 +0100

    add zsh completion for kill

--- dotfiles/zshrc.d/20-config.zsh
+++ dotfiles/zshrc.d/20-config.zsh
@@ -12,6 +12,7 @@ zstyle ':completion:*' completer _complete _match _approximate
 zstyle ':completion:*:match:*' original only
 zstyle -e ':completion:*:approximate:*' \
         max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+zstyle ':completion:*:*:*:*:processes' command "ps -u $(whoami) -o pid,user,comm -w -w"
 zstyle :compinstall filename "$HOME/.zshrc"
 
 autoload -Uz compinit
