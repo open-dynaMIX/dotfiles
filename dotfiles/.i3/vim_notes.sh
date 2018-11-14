e04b56daf634795b3e4ca018551e767c937caafe
Fabio Rämi <open-dynaMIX@users.noreply.github.com>
2018-11-14 11:37:41 +0100

    termite seems to have removed the --geometry option

--- dotfiles/.i3/vim_notes.sh
+++ dotfiles/.i3/vim_notes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # {{@@ env['dotdrop_warning'] @@}}
 
-termite -t vim_notes --geometry 770x550 -e "vim -c 'set wrap' -c 'set colorcolumn=0' $HOME/Notes.txt"
+termite -t vim_notes -e "vim -c 'set wrap' -c 'set colorcolumn=0' $HOME/Notes.txt"
