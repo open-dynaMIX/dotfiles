75600c9bb1cfce3cca695b6080a948d1962de2f1
Fabio <open-dynaMIX@users.noreply.github.com>
2019-01-22 17:25:44 +0100

    use develop branch of zsh-autosuggestions until the fix for #404 is in a release; set thresholds for volume_status

--- dotfiles/.zshrc.d/55-autosuggestions.zsh
+++ dotfiles/.zshrc.d/55-autosuggestions.zsh
@@ -6,5 +6,7 @@
 # Fix slow pasting
 zstyle ':bracketed-paste-magic' active-widgets '.self-*'
 
-. /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+. $HOME/code/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# . /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=59"
