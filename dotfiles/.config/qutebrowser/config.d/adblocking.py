b20638ae47a2c9bbfb603b563ce54a70ba57a94e
Fabio <open-dynaMIX@users.noreply.github.com>
2019-09-12 12:40:33 +0200

    update qutebrowser config

--- dotfiles/.config/qutebrowser/config.d/adblocking.py
+++ dotfiles/.config/qutebrowser/config.d/adblocking.py
@@ -5,17 +5,22 @@
 c = c  # noqa
 config = config  # noqa
 
-## List of URLs of lists which contain hosts to block.  The file can be
-## in one of the following formats:  - An `/etc/hosts`-like file - One
-## host per line - A zip-file of any of the above, with either only one
-## file, or a file named   `hosts` (with any extension).
-## Type: List of Url
-# c.content.host_blocking.lists = ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts']
-c.content.host_blocking.lists = ['file:///home/{{@@ env["USER"] @@}}/.config/qutebrowser/blocked_hosts', 'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts']
+## List of URLs of lists which contain hosts to block.
+## The file can be in one of the following formats:
+##  - An /etc/hosts-like file
+##  - One host per line
+##  - A zip-file of any of the above, with either only one file, or a file
+##    named hosts (with any extension).
+## It’s also possible to add a local file or directory via a file:// URL.
+## In case of a directory, all files in the directory are read as adblock lists.
+## The file ~/.config/qutebrowser/blocked-hosts is always read if it exists.
+# c.content.host_blocking.lists = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"]
 
-## List of domains that should always be loaded, despite being ad-
-## blocked. Domains may contain * and ? wildcards and are otherwise
-## required to exactly match the requested domain. Local domains are
-## always exempt from hostblocking.
-## Type: List of String
-# c.content.host_blocking.whitelist = ['piwik.org']
+## A list of patterns that should always be loaded, despite being ad-blocked.
+## Note this whitelists blocked hosts, not first-party URLs. As an example,
+## if example.org loads an ad from ads.example.org, the whitelisted host
+## should be ads.example.org. If you want to disable the adblocker on a given
+## page, use the content.host_blocking.enabled setting with a URL pattern
+## instead. Local domains are always exempt from hostblocking.
+## Type: List of UrlPattern
+# c.content.host_blocking.whitelist = []
