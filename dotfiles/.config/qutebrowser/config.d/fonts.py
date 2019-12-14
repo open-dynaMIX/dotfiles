946361d49dfe3718ca894eedd38200dc67546bc9
Fabio Ambauen <open-dynaMIX@users.noreply.github.com>
2019-12-14 10:00:31 +0100

    update default in qb config

--- dotfiles/.config/qutebrowser/config.d/fonts.py
+++ dotfiles/.config/qutebrowser/config.d/fonts.py
@@ -52,7 +52,7 @@ c.fonts.messages.warning = '12pt monospace'
 ## Default monospace fonts. Whenever "monospace" is used in a font
 ## setting, it's replaced with the fonts listed here.
 ## Type: Font
-# c.fonts.monospace = '"xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
+# c.fonts.monospace = 'Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
 c.fonts.monospace = '"Fira Code", "Droid Sans Mono", "xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
 
 ## Font used for prompts.
