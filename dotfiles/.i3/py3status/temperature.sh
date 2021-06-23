f295408e8c7543d716e108d4ec2201a92ac0038e
Fabio <1833932+open-dynaMIX@users.noreply.github.com>
2021-06-23 11:38:10 +0200

    fix csv url for water temp

--- dotfiles/.i3/py3status/temperature.sh
+++ dotfiles/.i3/py3status/temperature.sh
@@ -2,7 +2,7 @@
 # {{@@ header() @@}}
 
 hergiswil=$(curl -q "https://www.wiewarm.ch/api/bad/Schwimmbad_Hergiswil_?cacheScrew=592518" 2>/dev/null | jq -r '.becken | ."Vierwaldstättersee".temp')
-reuss=$(curl -q https://www.hydrodaten.admin.ch/graphs/2152/temperature_2152.csv 2>/dev/null | tail -n 1 | cut -d "," -f 2)
+reuss=$(curl -q "https://www.hydrodaten.admin.ch/lhg/az/dwh/csv/BAFU_2152_Wassertemperatur.csv" 2>/dev/null | tail -n 1 | cut -d "," -f 2)
 
 echo "Hergiswil: ${hergiswil}°C"
 echo "Reuss: ${reuss}°C"
