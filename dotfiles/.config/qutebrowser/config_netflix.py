d70ad966533c731d16884b6835460762c5399bae
Fabio Ambauen <1833932+open-dynaMIX@users.noreply.github.com>
2024-02-11 15:22:33 +0100

    add qnetflix

---
+++ dotfiles/.config/qutebrowser/config_netflix.py
@@ -0,0 +1,22 @@
+# {{@@ header() @@}}
+# flake8: noqa: E266
+## Documentation:
+##   qute://help/configuring.html
+##   qute://help/settings.html
+
+from pathlib import Path
+
+
+
+config.source("{{@@ env['HOME'] @@}}/.config/qutebrowser/config.py")
+
+c = c  # noqa
+config = config  # noqa
+
+c.url.default_page = "https://netflix.com"
+
+c.tabs.show = "never"
+c.statusbar.show = "never"
+c.scrolling.bar = "never"
+c.auto_save.session = False
+c.content.notifications.enabled = True
